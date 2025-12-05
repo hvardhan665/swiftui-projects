8a32b5698d73ee952ee5752b46d01f29d46a08e3
hvardhan665 <81683582+hvardhan665@users.noreply.github.com>
2025-12-05 16:54:42 +0530

    Add files via upload

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Harshvardhan on 25/03/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
